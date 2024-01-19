bfc288aec2f0c1e9f8353cd47580a4cae1847827
meozalat2022 <meozalat2002@hotmail.com>
2024-01-19 12:37:57 +0200

    fashion app

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import notifee, {EventType} from '@notifee/react-native';
+
+function onMessageReceived(message) {
+  notifee.displayNotification(JSON.parse(message.data.notifee));
+}
 
 AppRegistry.registerComponent(appName, () => App);
